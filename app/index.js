d4233e1586a24910487ced9029070768a49a7492
emad <emad.puursina@gmail.com>
2020-10-22 09:54:10 +0330

    app/index.js: setup server

--- app/index.js
+++ app/index.js
@@ -1,7 +1,17 @@
-
-
-
+const express = require("express");
+const app = express();
+const http = require("http");
 
 module.exports = class Application {
+    constructor() {
+        this.setupExpress();
+    }
+
+    setupExpress() {
+        const server = http.createServer(app);
+        server.listen(3000, () => {
+            console.log('Running on port 3000....');
+        });
+    }
 
 }
