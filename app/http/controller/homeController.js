d80ad26db3c4c4a626f24d717d04680925697961
emad <emad.puursina@gmail.com>
2020-10-23 12:15:10 +0330

    make and render home page

--- app/http/controller/homeController.js
+++ app/http/controller/homeController.js
@@ -2,7 +2,7 @@ const Controller = require("app/http/controller/controller");
 
 class HomeController extends Controller{
     index(req, res) {
-        res.json("homePage 2");
+        res.render("home");
     }
 }
 
