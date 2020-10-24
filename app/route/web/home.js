f5ac9088a8e78a1722240cb8bf0a076afb3069a0
emad <emad.puursina@gmail.com>
2020-10-24 18:45:27 +0330

    add login and register route handler

--- app/route/web/home.js
+++ app/route/web/home.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const homeController = require("app/http/controller/homeController");
+const loginController = require("app/http/controller/auth/loginController");
+const registerController = require("app/http/controller/auth/registerController");
 
 router.get("/", homeController.index);
+router.get("/login", loginController.showLoginForm);
+router.get("/register", registerController.showRegistrationForm);
 
 
 module.exports = router;
