13ebb4541ff114401493a5e204f6950ffa981c99
emad <emad.puursina@gmail.com>
2020-10-26 19:14:58 +0330

    write the method for generating and validating the recaptcha

--- app/http/controller/controller.js
+++ app/http/controller/controller.js
@@ -1,10 +1,30 @@
 const autoBind = require("auto-bind");
+var Recaptcha = require('express-recaptcha').RecaptchaV2;
 
-class Controller {
+module.exports =  class Controller {
     constructor() {
         autoBind(this);
+        this.recaptchaGenerator();
+    }
+
+    recaptchaGenerator() {
+        this.recaptcha = new Recaptcha(
+            '6LdPMtsZAAAAAKE-Txf2GYOyvxkCZ1NoAuq59UDW',
+            '6LdPMtsZAAAAAImnX6cZLBwivOQZPsnZkqVTSyt6',
+            { h1: 'fa' }
+        );
+    }
+
+    recaptchaValidation(req, res) {
+        return new Promise((resolve, reject) => {
+           this.recaptcha.verify(req, (err, data) => {
+               if(!err){
+                   resolve(true);
+               }else {
+                   req.flash("errors", "please fill the recaptcha correctlly!!");
+                   res.redirect(req.url);
+               }
+           }) 
+        })
     }
 }
-
-
-module.exports = Controller;
