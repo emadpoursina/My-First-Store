5f0ad01ab778ebbd48496c71f576b162438d16cb
emad <emad.puursina@gmail.com>
2020-11-01 11:04:20 +0330

    write the login functions

--- app/http/controller/auth/loginController.js
+++ app/http/controller/auth/loginController.js
@@ -1,6 +1,64 @@
-class LoginController {
+const Controller = require("app/http/controller/controller.js");
+const passport = require("passport");
+
+class LoginController extends Controller {
     showLoginForm(req, res) {
-        res.render("auth/login.ejs");
+        res.render("auth/login.ejs", { errors: req.flash("errors"), recaptcha: this.recaptcha.render() });
+    }
+
+    loginProcess(req, res) {
+        this.recaptchaValidation(req, res)
+            .then( (result) => {
+                this.dataValidation(req, res)
+                    .then( (result) => {
+                        if(result) {
+                            this.loginUser(req, res);
+                        }else{
+                            res.redirect("/login");
+                        }
+                    })
+                    .catch( (err) => {
+                        console.log(err);
+                        res.send(err);
+                    })
+            })
+            .catch( (err) => {
+                console.log(err);
+                res.send(err);
+            })
+    }
+
+    dataValidation(req) {
+        req.checkBody("email", "Enter a valid Email.").isEmail();
+        req.checkBody("password", 'password field can not be empty.').notEmpty();
+
+        return req.getValidationResult()
+            .then( (result) => {
+                const errors = result.array();
+                const message = [];
+                errors.forEach(error => {
+                   message.push(error.msg);
+                });
+
+                if(message.length > 0) {
+                    req.flash("errors", message);
+                    return false;
+                }else {
+                    return true;
+                }
+            })
+            .catch( (err) => {
+                console.log(err);
+                res.send(err);
+            })
+    }
+
+    loginUser(req, res){
+        passport.authenticate("local.login", {
+            successRedirect: "/",
+            failureRedirect: "/login",
+            failureFlash: true
+        })(req, res)
     }
 }
 
