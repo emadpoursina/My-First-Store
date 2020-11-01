f1da113c4247cbfb06db45b17233626a8dfc4bcd
emad <emad.puursina@gmail.com>
2020-11-01 11:03:44 +0330

    add local.login strategy

--- app/passport/passport-local.js
+++ app/passport/passport-local.js
@@ -35,3 +35,24 @@ passport.use("local.register", new localStrategy({
         })
     })
 }))
+
+passport.use("local.login", new localStrategy({
+    usernameField: "email",
+    passwordField: "password",
+    passReqToCallback: true
+}, (req, email, password, done) => {
+    User.findOne({ "email": email }, (err, user) => {
+        if(err){
+            done(err);
+        }
+
+        if(!user || user.comparePassword(password)){
+            return done(null, false, req.flash("errors", "The info is not correct"));
+        }
+
+        console.log("user", user);
+
+        return done(null, user);
+
+    })
+}))
