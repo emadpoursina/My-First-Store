8b6ef920b9a7ac5d4a2546e33cb45623f51c66ca
emad <emad.puursina@gmail.com>
2020-11-01 10:55:33 +0330

    handle login post request

--- app/route/web/home.js
+++ app/route/web/home.js
@@ -7,6 +7,7 @@ const registerController = require("app/http/controller/auth/registerController"
 
 router.get("/", homeController.index);
 router.get("/login", loginController.showLoginForm);
+router.post("/login", loginController.loginProcess);
 router.get("/register", registerController.showRegistrationForm);
 router.post("/register", registerController.registrationProcess);
 
