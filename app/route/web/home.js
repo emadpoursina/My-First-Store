eaad91974daf2b7e1d5dea6d06b28b5d8ac3c3f0
emad <emad.puursina@gmail.com>
2020-11-17 10:08:21 +0330

    load redirectIfAuthenticated middleware

--- app/route/web/home.js
+++ app/route/web/home.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+//Controllers
 const homeController = require("app/http/controller/homeController");
 const loginController = require("app/http/controller/auth/loginController");
 const registerController = require("app/http/controller/auth/registerController");
 
+//Middlewares
+const redirectIfAuthenticated = require("app/http/middleware/RedirectIfAuthenticated");
+
 router.get("/", homeController.index);
-router.get("/login", loginController.showLoginForm);
-router.post("/login", loginController.loginProcess);
-router.get("/register", registerController.showRegistrationForm);
-router.post("/register", registerController.registrationProcess);
+router.get("/login", redirectIfAuthenticated.handle, loginController.showLoginForm);
+router.post("/login", redirectIfAuthenticated.handle, loginController.loginProcess);
+router.get("/register", redirectIfAuthenticated.handle, registerController.showRegistrationForm);
+router.post("/register", redirectIfAuthenticated.handle, registerController.registrationProcess);
 
 router.get("/logout", (req, res) => {
     req.logOut();
