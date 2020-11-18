3f08a31e18c2fb6361dafd8d61437f020df105dd
emad <emad.puursina@gmail.com>
2020-11-18 18:41:25 +0330

    add database config

---
+++ config/database.js
@@ -0,0 +1,7 @@
+
+
+module.exports = {
+    url: "mongodb://localhost/frosh2D",
+    options: { useNewUrlParser: true, useUnifiedTopology: true },
+    successMessage: "mogoose is running ....."
+}   
