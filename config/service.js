abdddb33a650869f04d5cdc103bd6378fb399ef5
emad <emad.puursina@gmail.com>
2020-11-18 19:00:55 +0330

    add services/recaptcha config

---
+++ config/service.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+    recaptcha: {
+        client_key: "6LdPMtsZAAAAAKE-Txf2GYOyvxkCZ1NoAuq59UDW",
+        secret_key: "6LdPMtsZAAAAAImnX6cZLBwivOQZPsnZkqVTSyt6",
+        options: {h1: 'fa'}
+    }
+}
