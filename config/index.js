239faa0a29d473fdd0a774468424813dbf2a26ff
emad <emad.puursina@gmail.com>
2020-11-18 19:01:31 +0330

    add service config object

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 const application = require("./application");
 const database = require("./database");
 const session = require("./session");
+const service = require("./service");
 
 module.exports = {
     application,
     database,
-    session
-
+    session,
+    service
 }
