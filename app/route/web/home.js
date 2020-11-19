953f576866076348b7f58135b8017335fd618176
emad <emad.puursina@gmail.com>
2020-11-19 19:12:48 +0330

    add registration and login validation middlewares

--- app/route/web/home.js
+++ app/route/web/home.js
@@ -9,11 +9,15 @@ const registerController = require("app/http/controller/auth/registerController"
 //Middlewares
 const redirectIfAuthenticated = require("app/http/middleware/RedirectIfAuthenticated");
 
+//validators
+const regisetrValidator = require("app/http/validators/RegisterValidator");
+const loginValidator = require("app/http/validators/LoginValidators");
+
 router.get("/", homeController.index);
 router.get("/login", redirectIfAuthenticated.handle, loginController.showLoginForm);
-router.post("/login", redirectIfAuthenticated.handle, loginController.loginProcess);
+router.post("/login", redirectIfAuthenticated.handle, loginValidator.handle(), loginController.loginProcess);
 router.get("/register", redirectIfAuthenticated.handle, registerController.showRegistrationForm);
-router.post("/register", redirectIfAuthenticated.handle, registerController.registrationProcess);
+router.post("/register", redirectIfAuthenticated.handle, regisetrValidator.handle(), registerController.registrationProcess);
 
 router.get("/logout", (req, res) => {
     req.logOut();
