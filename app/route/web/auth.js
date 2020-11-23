12f39207d0e4f439cec2508c31962a1762facc5f
emad <emad.puursina@gmail.com>
2020-11-23 16:48:29 +0330

    answer to the google and user request

--- app/route/web/auth.js
+++ app/route/web/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 //Controller
 const loginController = require("app/http/controller/auth/loginController");
@@ -13,5 +14,8 @@ router.get("/login", loginController.showLoginForm);
 router.post("/login", loginValidator.handle(), loginController.loginProcess);
 router.get("/register", registerController.showRegistrationForm);
 router.post("/register", regisetrValidator.handle(), registerController.registrationProcess);
+
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"]}));
+router.get("/google/callback", passport.authenticate("google", { successRedirect: "/", failurRedirect: "/register"}));
 
 module.exports = router;
