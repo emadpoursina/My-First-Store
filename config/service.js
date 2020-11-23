4641d4c93f2753ff68c1bc1a72a6042a9462ec9a
emad <emad.puursina@gmail.com>
2020-11-23 16:47:07 +0330

    add 3 global variable

--- config/service.js
+++ config/service.js
@@ -5,5 +5,10 @@ module.exports = {
         client_key: process.env.RECAPTCHA_CLIENTKEY,
         secret_key: process.env.RECAPTCHA_SECRETKEY,
         options: {h1: 'fa'}
+    },
+    google: {
+        client_key: process.env.GOOGLE_CLIENTKEY,
+        secret_key: process.env.GOOGLE_SECRETKEY,
+        callback_url: process.env.GOOGLE_CALLBACKURL
     }
 }
