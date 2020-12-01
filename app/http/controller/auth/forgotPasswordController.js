abf605842eaec4eb9ec633e843cd199bb7ce06bc
emad <emad.puursina@gmail.com>
2020-12-01 10:08:12 +0330

    change forgot password controller and validator class and file

--- app/http/controller/auth/forgotPasswordController.js
+++ app/http/controller/auth/forgotPasswordController.js
@@ -1,10 +1,10 @@
 const Controller = require("./../controller");
 
-class ForgotPassword extends Controller {
+class ForgotPasswordController extends Controller {
     showForgotPassword (req, res){
         const title = "فراموشی رمز عبور";
         res.render("home/auth/password/forgotPassword.ejs", {errors: req.flash("errors"),  recaptcha: this.recaptcha.render(), title});
     }
 }
 
-module.exports = new ForgotPassword();
+module.exports = new ForgotPasswordController();
---
+++ app/http/validators/ForgotPasswordValidator.js
@@ -0,0 +1,15 @@
+const { validationResult } = require("express-validator/check");
+const Validator = require("./Validator");
+
+class ForgotPasswordValidator extends Validator{
+    handle (){
+        return [
+            check("email")
+                .isEmail()
+                .withMessage("ایمیل معتبر نیست");
+        ]
+
+    }
+}
+
+module.exports = new ForgotPasswordValidator();
