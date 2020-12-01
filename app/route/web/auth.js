5624b7df896441e0fc5b320a4b1406d2ecadb2c8
emad <emad.puursina@gmail.com>
2020-12-01 09:53:03 +0330

    load forgotPassword validator and post route respond

--- app/route/web/auth.js
+++ app/route/web/auth.js
@@ -5,18 +5,20 @@ const passport = require("passport");
 //Controller
 const loginController = require("app/http/controller/auth/loginController");
 const registerController = require("app/http/controller/auth/registerController");
-const forgotPassword = require("app/http/controller/auth/forgotPassword");
+const forgotPasswordController = require("app/http/controller/auth/forgotPasswordController");
 
 //validators
 const regisetrValidator = require("app/http/validators/RegisterValidator");
 const loginValidator = require("app/http/validators/LoginValidators");
+const forgotPasswordValidator = require("app/http/validators/ForgotPassword");
 
 router.get("/login", loginController.showLoginForm);
 router.post("/login", loginValidator.handle(), loginController.loginProcess);
 router.get("/register", registerController.showRegistrationForm);
 router.post("/register", regisetrValidator.handle(), registerController.registrationProcess);
 
-router.get("/password/reset", forgotPassword.showForgotPassword);
+router.get("/password/reset", forgotPasswordController.showForgotPassword);
+router.post("/password/email", forgotPasswordValidator.handle(), forgotPasswordController.sendPasswordResetLink);
 
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"]}));
 router.get("/google/callback", passport.authenticate("google", { successRedirect: "/", failurRedirect: "/register"}));
