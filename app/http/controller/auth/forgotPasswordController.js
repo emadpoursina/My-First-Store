172eddf72b9d73c344718ad868933d881b5dde96
emad <emad.puursina@gmail.com>
2020-12-01 12:26:16 +0330

    validate email and recaptcha

--- app/http/controller/auth/forgotPasswordController.js
+++ app/http/controller/auth/forgotPasswordController.js
@@ -1,10 +1,23 @@
 const Controller = require("./../controller");
+const uniqueString = require("unique-string");
+const PasswordReset = require("app/model/password-reset");
 
 class ForgotPasswordController extends Controller {
     showForgotPassword (req, res){
         const title = "فراموشی رمز عبور";
         res.render("home/auth/password/forgotPassword.ejs", {errors: req.flash("errors"),  recaptcha: this.recaptcha.render(), title});
     }
+
+    async sendPasswordResetLink (req, res){
+        await this.recaptchaValidation();
+        const result = await this.validateData(req);
+        if (!result){
+            res.redirect("/auth/password/reset");
+        }
+
+        return res.sendResetLink(req);
+    }
+
 }
 
 module.exports = new ForgotPasswordController();
