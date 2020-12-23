2ac3d5121c1f394680a707e8e935582ea15610dc
emad <emad.puursina@gmail.com>
2020-12-23 11:35:38 +0330

    Beautification

--- app/route/web/index.js
+++ app/route/web/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+//Middlewares
+const redirectIfAuthenticated = require("app/http/middleware/RedirectIfAuthenticated");
+const authRouter = require("app/route/web/auth");
+router.use("/auth", redirectIfAuthenticated.handle, authRouter);
+
 /* Handle all of the route for the admin area */
 const adminRouter = require("app/route/web/admin");
 router.use("/admin", adminRouter);
@@ -8,10 +13,5 @@ router.use("/admin", adminRouter);
 /* Handle all of the public area */
 const homeRouter = require("app/route/web/home");
 router.use("/", homeRouter);
-
-//Middlewares
-const redirectIfAuthenticated = require("app/http/middleware/RedirectIfAuthenticated");
-const authRouter = require("app/route/web/auth");
-router.use("/auth", redirectIfAuthenticated.handle, authRouter);
 
 module.exports = router;
