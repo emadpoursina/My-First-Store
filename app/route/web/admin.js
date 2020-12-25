ef2a4e36e65c26bc30c8fdf334d18080a00513e4
emad <emad.puursina@gmail.com>
2020-12-25 12:30:56 +0330

    load course route in admin panel

--- app/route/web/admin.js
+++ app/route/web/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const adminController = require("app/http/controller/admin/adminController");
+const courseController = require("app/http/controller/admin/courseController");
 
 router.use((req, res, next) => {
     res.locals.layout = "admin/master";
@@ -10,5 +11,7 @@ router.use((req, res, next) => {
 })
 
 router.get("/", adminController.index);
+router.get("/courses", courseController.index);
+router.get("/courses/create", courseController.creat);
 
 module.exports = router;
