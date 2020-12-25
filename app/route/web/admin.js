baeaedbb9fd4284a1a79b7b89ac5224776294faa
emad <emad.puursina@gmail.com>
2020-12-25 11:56:59 +0330

    load the admin panel

--- app/route/web/admin.js
+++ app/route/web/admin.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const adminController = require("app/http/controller/admin/adminController");
 
+router.use((req, res, next) => {
+    res.locals.layout = "admin/master";
+
+    next();
+})
+
 router.get("/", adminController.index);
 
 module.exports = router;
