836996dd0977cd66bad2ec1e40850e8df365b342
emad <emad.puursina@gmail.com>
2020-12-30 12:06:15 +0330

    handle creating a course

--- app/http/controller/admin/courseController.js
+++ app/http/controller/admin/courseController.js
@@ -1,4 +1,5 @@
 const Controller = require("./../controller");
+const Course = require("app/model/Course");
 
 class CourseController extends Controller {
     index(req, res) {
@@ -7,6 +8,34 @@ class CourseController extends Controller {
 
     creat(req, res) {
         res.render("admin/courses/creat");
+    }
+
+    async store(req, res) {
+        const result = await this.validateData(req);
+        if(!result) {
+            return this.back(req, res);
+        }
+
+        const images = req.body.images;
+        const { title , body , type , price , tags } = req.body;
+
+        const newCourse = new Course({
+            user: req.user._id,
+            title,
+            body,
+            slug: this.slug(title),
+            type,
+            price,
+            tags,
+            images,
+        }); 
+
+        await newCourse.save();
+        return res.redirect("/admin/courses");
+    }
+
+    slug(title) {
+        return title.replace(/([^۰-۹آ-یa-z0-9]|-)+/g , "-")
     }
 }
 
