f09b950f8275a486537da670cf9c56741e72d355
emad <emad.puursina@gmail.com>
2021-01-05 09:55:43 +0330

    load a middlewate and beutification

--- app/route/web/admin.js
+++ app/route/web/admin.js
@@ -8,19 +8,24 @@ const courseController = require("app/http/controller/admin/courseController");
 // Validators
 const courseValidator = require("app/http/validators/CourseValidator");
 
-
 //Middlewares
 const uploadImage = require("app/helpers/uploadImage.js") //helpers
+const convertFileToField = require("app/http/middleware/ConvertFileToField.js");
 
 router.use((req, res, next) => {
-    res.locals.layout = "admin/master";
+  res.locals.layout = "admin/master";
 
-    next();
+  next();
 })
 
 router.get("/", adminController.index);
 router.get("/courses", courseController.index);
 router.get("/courses/create", courseController.creat);
-router.post("/courses/create", uploadImage.single("images"), courseValidator.handle(), courseController.store);
+router.post("/courses/create",
+  uploadImage.single("images"),
+  convertFileToField.handle,
+  courseValidator.handle(),
+  courseController.store
+);
 
 module.exports = router;
