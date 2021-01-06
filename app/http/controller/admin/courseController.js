14843a8efd66c22090784c86c188065a3723b572
emad <emad.puursina@gmail.com>
2021-01-06 08:50:11 +0330

    define resize function, load modules and beautification

--- app/http/controller/admin/courseController.js
+++ app/http/controller/admin/courseController.js
@@ -1,45 +1,51 @@
 const Controller = require("./../controller");
 const Course = require("app/model/Course");
 const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
 
 class CourseController extends Controller {
-    index(req, res) {
-      res.render("admin/courses/index", {title: "دوره ها"});
+  index(req, res) {
+    res.render("admin/courses/index", {title: "دوره ها"});
+  }
+
+  creat(req, res) {
+    res.render("admin/courses/creat");
+  }
+
+  async store(req, res) {
+    const result = await this.validateData(req);
+    if(!result) {
+      if(req.file)
+        fs.unlinkSync(req.file.path);
+      return this.back(req, res);
     }
 
-    creat(req, res) {
-      res.render("admin/courses/creat");
-    }
+    const images = this.imageResize(req.file);
+    const { title , body , type , price , tags } = req.body;
 
-    async store(req, res) {
-        const result = await this.validateData(req);
-        if(!result) {
-            if(req.file)
-              fs.unlinkSync(req.file.path);
-            return this.back(req, res);
-        }
+    const newCourse = new Course({
+      user: req.user._id,
+      title,
+      body,
+      slug: this.slug(title),
+      type,
+      price,
+      tags,
+      images,
+    }); 
 
-        const images = req.body.images;
-        const { title , body , type , price , tags } = req.body;
+    await newCourse.save();
+    return res.redirect("/admin/courses");
+  }
 
-        const newCourse = new Course({
-            user: req.user._id,
-            title,
-            body,
-            slug: this.slug(title),
-            type,
-            price,
-            tags,
-            images,
-        }); 
+  slug(title) {
+    return title.replace(/([^۰-۹آ-یa-z0-9]|-)+/g , "-")
+  }
 
-        await newCourse.save();
-        return res.redirect("/admin/courses");
-    }
+  imageResize(image) {
 
-    slug(title) {
-        return title.replace(/([^۰-۹آ-یa-z0-9]|-)+/g , "-")
-    }
+  }
 }
 
 module.exports = new CourseController();
