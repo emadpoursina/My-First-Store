d8be971a8f7b0bd7e3ad340e5d5c866a1031d256
emad <emad.puursina@gmail.com>
2021-01-08 11:16:26 +0330

    some beautification

--- app/helpers/uploadImage.js
+++ app/helpers/uploadImage.js
@@ -2,30 +2,29 @@ const multer = require("multer");
 const mkdirp = require("mkdirp");
 const fs = require("fs");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const dir = getDirImage();
+
+    mkdirp(dir)
+    .then(made => cb(null, dir))
+    .catch(err => console.error(err));
+  },
+  filename: function (req, file, cb) {
+    const newFile = getDirImage() + "/" + file.originalname;
+    if(fs.existsSync(newFile))
+      file.originalname = Date.now() + "-" + file.originalname;
+
+    cb(null, file.originalname);
+  }
+});
+
 const getDirImage = () => {
   const year = new Date().getFullYear();
   const month = new Date().getMonth() + 1;
   const day = new Date().getDay();
   return `./public/upload/${year}/${month}/${day}`;
 }
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      const dir = getDirImage();
-
-      mkdirp(dir)
-        .then(made => cb(null, dir))
-        .catch(err => console.error(err));
-    },
-    filename: function (req, file, cb) {
-      const newFile = getDirImage() + "/" + file.originalname;
-      if(fs.existsSync(newFile))
-        file.originalname = Date.now() + "-" + file.originalname;
-
-      cb(null, file.originalname);
-
-    }
-  });
 
 const uploadImage = multer({
   storage
