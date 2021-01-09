22b5a3d22852251740d73b95fbed0f8767fe3f63
emad <emad.puursina@gmail.com>
2021-01-09 20:29:35 +0330

    Beautification

--- app/http/controller/controller.js
+++ app/http/controller/controller.js
@@ -5,61 +5,61 @@ const { validationResult } = require("express-validator/check");
 const isMongoId = require("validator/lib/isMongoId");
 
 module.exports =  class Controller {
-		constructor() {
-				autoBind(this);
-				this.recaptchaGenerator();
-		}
+	constructor() {
+		autoBind(this);
+		this.recaptchaGenerator();
+	}
 
-		recaptchaGenerator() {
-				this.recaptcha = new Recaptcha(
-						config.service.recaptcha.client_key,
-						config.service.recaptcha.secret_key,
-						config.service.recaptcha.options
-				);
-		}
+	recaptchaGenerator() {
+		this.recaptcha = new Recaptcha(
+			config.service.recaptcha.client_key,
+			config.service.recaptcha.secret_key,
+			config.service.recaptcha.options
+		);
+	}
 
-		recaptchaValidation(req, res) {
-				return new Promise((resolve, reject) => {
-					 this.recaptcha.verify(req, (err, data) => {
-							 if(!err){
-									 resolve(true);
-							 }else {
-									 req.flash("errors", "ریکپچا را به درستی وارد کنید");
-									 this.back(req, res);
-							 }
-					 }) 
-				})
-		}
-
-		async validateData(req) {
-				const errors = validationResult(req);
-				if(! errors.isEmpty()){
-						let message = [];
-
-						errors.array().forEach(error => {
-							 message.push(error.msg);
-						});
-
-						req.flash("errors", message);
-						return false;
+	recaptchaValidation(req, res) {
+		return new Promise((resolve, reject) => {
+			this.recaptcha.verify(req, (err, data) => {
+				if(!err){
+					resolve(true);
+				}else {
+					req.flash("errors", "ریکپچا را به درستی وارد کنید");
+					this.back(req, res);
 				}
-				return true;
-		}
+			}); 
+		});
+	}
 
-		back(req, res) {
-				req.flash("formdata", req.body);
-				return res.redirect(req.header("Referer") || "/");
-		}
+	async validateData(req) {
+		const errors = validationResult(req);
+		if(! errors.isEmpty()){
+			let message = [];
 
-		isMongoId(mongoId) {
-			if(! isMongoId(mongoId)) {
-				this.error("Invalid Id", 404);
-			}
-		}
+			errors.array().forEach(error => {
+				message.push(error.msg);
+			});
 
-		error(message, statusCode) {
-			const err = new Error(message);
-			err.statusCode = statusCode;
-			throw err;
-		} 
+			req.flash("errors", message);
+			return false;
+		}
+		return true;
+	}
+
+	back(req, res) {
+		req.flash("formdata", req.body);
+		return res.redirect(req.header("Referer") || "/");
+	}
+
+	isMongoId(mongoId) {
+		if(! isMongoId(mongoId)) {
+			this.error("Invalid Id", 404);
+		}
+	}
+
+	error(message, statusCode) {
+		const err = new Error(message);
+		err.statusCode = statusCode;
+		throw err;
+	} 
 }
