0b24c187846ca29cfd398c158192d2fdc720ccd0
emadPoursina <emad.puursina@gmail.com>
2021-01-28 18:18:43 +0330

    add store function

--- app/http/controller/admin/EpisodeController.js
+++ app/http/controller/admin/EpisodeController.js
@@ -11,6 +11,18 @@ class EpisodeController extends Controller {
         const courses = await require('app/model/Course').find();
         res.render('admin/episodes/create', {title: 'ساخت قسمت جدید', courses})
     }
+
+    async store(req, res) {
+        const status = this.validateData(req);
+        if(!status) {
+            this.back(req, res);
+        }
+
+        const newCourse = new Episode({ ...req.body });
+
+        await newCourse.save();
+        return res.redirect('/admin/episodes');
+    }
 }
 
 module.exports = new EpisodeController();
