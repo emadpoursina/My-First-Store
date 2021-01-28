1235bd8fc7bdf6c9d3679b5e0de9bbbaaf51e719
emadPoursina <emad.puursina@gmail.com>
2021-01-28 11:47:44 +0330

    add EpisodeController

---
+++ app/http/controller/admin/EpisodeController.js
@@ -0,0 +1,7 @@
+const Controller = require('../controller');
+const Episode = require('app/model/Episode');
+
+class EpisodeController extends Controller {
+}
+
+module.exports = new EpisodeController();
