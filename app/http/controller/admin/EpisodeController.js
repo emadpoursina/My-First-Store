6f59a81eb437a77743a7296fb9e2b734891204a5
emadPoursina <emad.puursina@gmail.com>
2021-01-28 12:18:07 +0330

    add index function to EpisodeController

--- app/http/controller/admin/EpisodeController.js
+++ app/http/controller/admin/EpisodeController.js
@@ -2,6 +2,10 @@ const Controller = require('../controller');
 const Episode = require('app/model/Episode');
 
 class EpisodeController extends Controller {
+    async index(req, res, next) {
+        const episodes = await Episode.paginate({}, {limit: 10});
+        res.render('admin/episodes/index', {title: 'قسمت ها', episodes})
+    }
 }
 
 module.exports = new EpisodeController();
