d08ccc00225e6daeedad8c3d723c87d49c4b6439
emadPoursina <emad.puursina@gmail.com>
2021-01-28 11:36:23 +0330

    add episode model

---
+++ app/model/Episode.js
@@ -0,0 +1,16 @@
+const  mongoose = require("mongoose");
+
+const episodeSchema = mongoose.Schema({
+    course: {type: mongoose.Schema.Types.ObjectId, ref: 'Course'},
+    title: {type: String, required: true},
+    type: {type: String, required: true},
+    body: {type: String, required: true},
+    time: {type: String, default: "00:00:00"},
+    number: {type: Number, required: true},
+    videoUrl: {type: String, required: true},
+    downloadCoutn: {type: Number, default: 0},
+    viewCount: {type: Number, default: 0},
+    commentCount: {type: Number, default: 0},
+}, {timestamps: true});
+
+module.exports = new mongoose.model('Episode', episodeSchema);
