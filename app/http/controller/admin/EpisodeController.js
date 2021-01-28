8bf1f1aebf653db692f784dd2765bb9d61daa3a4
emadPoursina <emad.puursina@gmail.com>
2021-01-28 13:01:03 +0330

    add create function to episodeController

--- app/http/controller/admin/EpisodeController.js
+++ app/http/controller/admin/EpisodeController.js
@@ -2,9 +2,14 @@ const Controller = require('../controller');
 const Episode = require('app/model/Episode');
 
 class EpisodeController extends Controller {
-    async index(req, res, next) {
+    async index(req, res) {
         const episodes = await Episode.paginate({}, {limit: 10});
         res.render('admin/episodes/index', {title: 'قسمت ها', episodes})
+    }
+
+    async create(req, res, next) {
+        const courses = await require('app/model/Course').find();
+        res.render('admin/episodes/create', {title: 'ساخت قسمت جدید', courses})
     }
 }
 
