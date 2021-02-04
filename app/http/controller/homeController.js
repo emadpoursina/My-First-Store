2a8cbc061b39dbf3c2fe21e1684515952c4f5007
emadPoursina <emad.puursina@gmail.com>
2021-02-04 22:31:51 +0330

    send course list to index view

--- app/http/controller/homeController.js
+++ app/http/controller/homeController.js
@@ -1,14 +1,15 @@
 const Controller = require("app/http/controller/controller");
+const Course = require('app/model/Course');
 
 class HomeController extends Controller{
-    index(req, res) {
-        res.render("home/", {title: "Home"});
+    async index(req, res) {
+        const courses = await Course.find({}).sort({ createdAt: 1 }).limit(8).exec();
+        res.render("home/", {title: "Home", courses});
     }
 
     about(req, res) {
         res.render("home/about", {title: 'aboutme'});
     }
 }
-
 
 module.exports = new HomeController();
