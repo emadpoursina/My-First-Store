c6e9a7c877a7afef275965ac4656e513ea12eda4
emadPoursina <emad.puursina@gmail.com>
2021-02-09 16:02:21 +0330

    some indention

--- app/http/middleware/RedirectIfAuthenticated.js
+++ app/http/middleware/RedirectIfAuthenticated.js
@@ -1,12 +1,12 @@
 const Middleware = require("./middleware");
 
 class RedirectIfAuthenticated extends Middleware {
-    handle(req, res, next) {
-        if (req.isAuthenticated()) {
-            return res.redirect("/");
-        }
-        next();
-    }
+	handle(req, res, next) {
+		if (req.isAuthenticated()) {
+			return res.redirect("/");
+		}
+		next();
+	}
 }
 
 module.exports = new RedirectIfAuthenticated();
