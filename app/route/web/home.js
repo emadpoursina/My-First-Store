25de16a5d98100bf03d8ae84acdccc0f14574197
emadPoursina <emad.puursina@gmail.com>
2021-02-09 15:53:54 +0330

    load comment validator and error layout

--- app/route/web/home.js
+++ app/route/web/home.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const homeController = require("app/http/controller/homeController");
 const courseController = require('app/http/controller/CourseController');
 
+//Vlidators
+const commentValidator = require('app/http/validators/CommentValidator');
+
 router.get("/logout", (req, res) => {
     req.logOut();
     res.clearCookie("remember_token");
@@ -16,6 +19,6 @@ router.get("/about-me", homeController.about);
 router.get("/courses", courseController.index);
 router.get("/courses/:course", courseController.single);
 router.get("/download/:id", courseController.download);
-router.post("/comment", homeController.comment);
+router.post("/comment", commentValidator.handle(),  homeController.comment);
 
 module.exports = router;
