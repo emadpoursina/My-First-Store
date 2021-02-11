5f9b29b0360c47bafaeb41cc63a06079785f787c
emadPoursina <emad.puursina@gmail.com>
2021-02-11 09:33:14 +0330

    some indention

--- app/model/Episode.js
+++ app/model/Episode.js
@@ -3,16 +3,16 @@ const mongoosePaginate = require('mongoose-paginate');
 const bcrypt = require('bcrypt');
 
 const episodeSchema = mongoose.Schema({
-    course: {type: mongoose.Schema.Types.ObjectId, ref: 'Course'},
-    title: {type: String, required: true},
-    type: {type: String, required: true},
-    body: {type: String, required: true},
-    time: {type: String, default: "00:00:00"},
-    number: {type: Number, required: true},
-    videoUrl: {type: String, required: true},
-    downloadCoutn: {type: Number, default: 0},
-    viewCount: {type: Number, default: 0},
-    commentCount: {type: Number, default: 0},
+  course: {type: mongoose.Schema.Types.ObjectId, ref: 'Course'},
+  title: {type: String, required: true},
+  type: {type: String, required: true},
+  body: {type: String, required: true},
+  time: {type: String, default: "00:00:00"},
+  number: {type: Number, required: true},
+  videoUrl: {type: String, required: true},
+  downloadCoutn: {type: Number, default: 0},
+  viewCount: {type: Number, default: 0},
+  commentCount: {type: Number, default: 0},
 }, {timestamps: true});
 
 episodeSchema.plugin(mongoosePaginate);
