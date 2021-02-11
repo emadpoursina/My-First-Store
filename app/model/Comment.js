440fb37ac58df91e89c2e753fef83d9a4e5a68e6
emadPoursina <emad.puursina@gmail.com>
2021-02-11 08:35:50 +0330

    fix some typo and add an option

--- app/model/Comment.js
+++ app/model/Comment.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate');
 
 const CommentSchema = new mongoose.Schema({
-  user: { type: mongoose.SchemaTypes.ObjectId, ref: 'user'},
-  parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'comment', default: null},
-  approved: {type: Boolean, default: false},
-  course: { type: mongoose.SchemaTypes.ObjectId, ref: 'course', default: undefined},
-  episode: { type: mongoose.SchemaTypes.ObjectId, ref: 'episode', default: undefined},
-}, { timestamps: true });
+  user: { type: mongoose.SchemaTypes.ObjectId, ref: 'User'},
+  parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Comment', default: null},
+  approved: {type: Boolean, default: true},
+  course: { type: mongoose.SchemaTypes.ObjectId, ref: 'Course', default: undefined},
+  episode: { type: mongoose.SchemaTypes.ObjectId, ref: 'Episode', default: undefined},
+  comment: {type: String, required: true },
+}, {timestamps: true, toJSON: { virtuals: true }});
 
 CommentSchema.plugin(mongoosePaginate);
 
