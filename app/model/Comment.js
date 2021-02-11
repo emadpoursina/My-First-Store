93107809956519e66fc7fb0f1136b089a1a3837b
emadPoursina <emad.puursina@gmail.com>
2021-02-11 11:00:24 +0330

    change a typo

--- app/model/Comment.js
+++ app/model/Comment.js
@@ -4,7 +4,7 @@ const mongoosePaginate = require('mongoose-paginate');
 const CommentSchema = new mongoose.Schema({
   user: { type: mongoose.SchemaTypes.ObjectId, ref: 'User'},
   parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Comment', default: null},
-  approved: {type: Boolean, default: true},
+  approved: {type: Boolean, default: false},
   course: { type: mongoose.SchemaTypes.ObjectId, ref: 'Course', default: undefined},
   episode: { type: mongoose.SchemaTypes.ObjectId, ref: 'Episode', default: undefined},
   comment: {type: String, required: true },
