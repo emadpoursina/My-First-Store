3acffe0ce1a1d2ca237b2420f1407b5910f9eb56
emadPoursina <emad.puursina@gmail.com>
2021-02-14 16:33:09 +0330

    add index method

--- app/http/controller/admin/CategoryController.js
+++ app/http/controller/admin/CategoryController.js
@@ -1,7 +1,16 @@
 const controller = require('./../controller');
+const Category = require('app/model/Category');
 
 class CategoryController extends controller {
+  async index(req, res, next) {
+    try {
+      const page = req.query.page | 1;
+      const categories = await Category.paginate({}, { page, limit: 10 });
+      res.render('admin/category/index', { title: 'دسته ها', categories });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = new CategoryController();
-
