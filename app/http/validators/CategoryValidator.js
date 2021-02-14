0db574d0177e92a3247444094a1e3684a567bae4
emadPoursina <emad.puursina@gmail.com>
2021-02-14 19:22:09 +0330

    validate name

--- app/http/validators/CategoryValidator.js
+++ app/http/validators/CategoryValidator.js
@@ -1,6 +1,22 @@
 const Validator = require('./Validator');
+const Category = require('app/model/Category');
+const { check } = require('express-validator/check');
 
 class CategoryValidator extends Validator {
+  handle() {
+    return [
+      check("name")
+				.isLength({ min: 3, max: 50 })
+        .withMessage('طول نام دسته نمی تواند کمتر از  دو کاراکتر و کمتر از پنجاه کاراکتر باشد.')
+        .custom(async (value) => {
+          const category = await Category.findOne({ name: value });
+          if(category){
+            throw new Error('نام دسته تکراری است');
+          }
+          return;
+        }),
+    ];
+  }
 }
 
 module.exports = new CategoryValidator();
