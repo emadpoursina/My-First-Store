e8517356536535a2dab0984d8f8519cc105b89cb
emadPoursina <emad.puursina@gmail.com>
2021-03-09 20:03:54 +0330

    send and show user info in panel

--- app/http/controller/UserController.js
+++ app/http/controller/UserController.js
@@ -2,7 +2,7 @@ const Controller = require('./controller');
 
 class UserController extends Controller {
   index(req, res, next) {
-   res.render('home/panel/index'); 
+    res.render('home/panel/index', { title: 'پنل کاربری', user: req.user }); 
   }  
 }
 
