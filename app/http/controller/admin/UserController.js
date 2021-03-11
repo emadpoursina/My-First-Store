a0033e69a9ea908a95387467d67d8d552ca9ea18
emadPoursina <emad.puursina@gmail.com>
2021-03-11 06:41:15 +0330

    delete extra codes and render user index

--- app/http/controller/admin/UserController.js
+++ app/http/controller/admin/UserController.js
@@ -3,7 +3,7 @@ const Controller = require('../controller');
 class UserController extends Controller {
   index(req, res, next) {
     try {
-      
+      res.render('admin/users/index');
     } catch (error) {
       next(error);
     }
