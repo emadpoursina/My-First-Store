dc677207becedfd7d1a43bfca66f96342dda475f
emadPoursina <emad.puursina@gmail.com>
2021-03-11 11:43:43 +0330

    fix a mistake and add a custom validation

--- app/http/validators/UserValidator.js
+++ app/http/validators/UserValidator.js
@@ -1,15 +1,21 @@
 const Validator = require('./Validator');
 const { check } = require('express-validator/check');
+const User = require('app/model/user');
 
 class UserValidator extends Validator {
   handle(req, res) {
     return [
       check('name')
         .isLength({ max: 20, min: 3 })
-        .withMessage('طول نام نمی تواند بیشتر از 10 کاراکتر باشد.'),
+        .withMessage('طول نام نمی تواند کم تر از ۳ و بیشتر از 20 کاراکتر باشد.'),
       check('email')
         .isLength({ max: 50 })
-        .withMessage('طول ایمیل نمی تواند بیشتر از 10 کاراکتر باشد.'),
+        .withMessage('طول ایمیل نمی تواند بیشتر از 10 کاراکتر باشد.')
+        .custom(async (value) => {
+          const user = await User.findOne({ email: value });
+          if(user)
+            throw new Error('این ایمیل تکرار ی است.');
+        }),
       check('password')
         .isLength({ min: 5 })
         .withMessage('طول رمز عبور نمی تواند کم تر از 5 کاراکتر باشد.'),
