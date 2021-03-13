7e0e0dfac6236499af9b50d26a5856422ddcc666
emadPoursina <emad.puursina@gmail.com>
2021-03-13 12:51:34 +0330

    send and show user payments

--- app/http/controller/UserController.js
+++ app/http/controller/UserController.js
@@ -1,12 +1,15 @@
 const Controller = require('./controller');
+const Payment = require('app/model/Payment');
 
 class UserController extends Controller {
   index(req, res, next) {
     res.render('home/panel/index', { title: 'پنل کاربری', user: req.user }); 
   }
 
-  history(req, res, next) {
-    res.render('home/panel/history', { title: 'پنل کاربری'})
+  async history(req, res, next) {
+    const payments = await Payment.find({ user: req.user._id }).populate('product');
+
+    res.render('home/panel/history', { title: 'پنل کاربری', payments})
   }
 }
 
