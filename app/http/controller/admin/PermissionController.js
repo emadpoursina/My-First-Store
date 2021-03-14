74c023dc6663cde99caae4a0540df5ce133bbd33
emadPoursina <emad.puursina@gmail.com>
2021-03-14 09:29:43 +0330

    add index method

--- app/http/controller/admin/PermissionController.js
+++ app/http/controller/admin/PermissionController.js
@@ -1,6 +1,17 @@
 const Controller = require('../controller');
+const Permission = require('app/model/Permission');
 
 class PermissionController extends Controller {
+  async index(req, res, next) {
+    try {
+      const page = req.query.page || 1;
+      const permissions = await Permission.paginate({}, { page, limit: 20 });
+
+      res.render('admin/permissions/index', { title: 'قابلیت ها', permissions});
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = new PermissionController();
