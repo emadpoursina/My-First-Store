ec055afe177846c47b332ca9b5fbf3f9d15261c6
emadPoursina <emad.puursina@gmail.com>
2021-03-14 10:02:17 +0330

    add store method

--- app/http/controller/admin/PermissionController.js
+++ app/http/controller/admin/PermissionController.js
@@ -20,6 +20,25 @@ class PermissionController extends Controller {
       next(error);
     }
   }
+
+  async store(req, res, next) {
+    try {
+      const result = this.validateData(req);
+      if(!result) {
+        return this.back(req, res);
+      }
+
+      const newPermission = new Permission({
+        ...req.body
+      });
+
+      await newPermission.save();
+
+      res.redirect('/admin/users/permissions');
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = new PermissionController();
