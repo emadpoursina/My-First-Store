916b9d3b878a0b85c7723083357ccd6c57fb1a77
emadPoursina <emad.puursina@gmail.com>
2021-03-14 09:12:55 +0330

    add permission model

---
+++ app/model/Permission.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const Schema = mongoose.Schema;
+
+const permissionSchema = new Schema({
+  name: { type: String, required: true },
+  label: { type: String, required: true },
+}, { timestamps: true, toJSON:{ virtuals: true } });
+
+permissionSchema.plugin(mongoosePaginate);
+
+module.exports = mognoose.model('Permission', permissionSchema);
