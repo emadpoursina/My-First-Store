8802d712542c236eb2589de132fef8f4469566db
emadPoursina <emad.puursina@gmail.com>
2021-03-15 12:12:45 +0330

    add create method

--- app/http/controller/admin/RoleController.js
+++ app/http/controller/admin/RoleController.js
@@ -1,7 +1,18 @@
 const controller = require('./../controller');
 const Role = require('app/model/Role');
+const Permission = require('app/model/Permission');
 
 class RoleController extends controller {
+  async create(req, res, next) {
+    try {
+      const permissions = await Permission.find();
+      const permissionOptions = this.makeOptions(permissions, 'label', '_id', true );
+
+      res.render('admin/roles/create', { title: 'ویرایش نقش', permissions: permissionOptions });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = new RoleController();
