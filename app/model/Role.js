40c38e3e3e01023cb08c34e8660c88a7469f7dee
emadPoursina <emad.puursina@gmail.com>
2021-03-15 11:32:02 +0330

    add Role model file and class

---
+++ app/model/Role.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const RoleSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  label: { type: String, required: true },
+  permissions: { type: [mongoose.SchemaTypes.ObjectId], ref: 'Permission' },
+}, { timestamps: true, toJSON: { virtuals: true }});
+
+RoleSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Role', RoleSchema);
