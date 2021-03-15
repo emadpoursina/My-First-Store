518c2034937d4773a7fa3fadd03284e32610ada4
emadPoursina <emad.puursina@gmail.com>
2021-03-15 12:31:07 +0330

    add role validator and handle method

---
+++ app/http/validators/RoleValidator.js
@@ -0,0 +1,31 @@
+const Validator = require('./Validator');
+const { check } = require('express-validator/check');
+const Role = require('app/model/Role');
+
+class RoleValidator extends Validator {
+  handle(){
+    return [
+      check('name')
+        .isLength({ min: 3, max: 50 })
+        .withMessage('طول نام باید بین ۳ تا ۲۰ کاراکتر باشد.')
+        .custom(async (value, { req }) => {
+          if(req.method === 'put') {
+            const currentRole = await Role.findById(req.params.id);
+            if(currentRole) return;
+          }
+
+          const newRole = await Role.findOne({ name: value });
+          if(newRole)
+            throw new Error('نام نقش نمی تواند تکراری باشد.');
+        }),
+      check('label')
+        .not().isEmpty()
+        .withMessage('توضیحات نمی تواند خالی باشد'),
+      check('permissions')
+        .not().isEmpty()
+        .withMessage('قابلیت ها نمی تواند خالی باشد'),
+    ];
+  }
+}
+
+module.exports = new RoleValidator();
