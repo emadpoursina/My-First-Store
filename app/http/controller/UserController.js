67c5a36c537eb7b79b1e7c613840ca7079d0d3bd
emadPoursina <emad.puursina@gmail.com>
2021-03-23 18:10:59 +0430

    add payment method

--- app/http/controller/UserController.js
+++ app/http/controller/UserController.js
@@ -1,5 +1,6 @@
 const Controller = require('./controller');
 const Payment = require('app/model/Payment');
+const axios = require('axios');
 
 class UserController extends Controller {
   index(req, res, next) {
@@ -19,6 +20,53 @@ class UserController extends Controller {
       next(error);
     }
   }
+
+  payment(req, res, next) {
+    try {
+      // Redirect user to payment gate
+      const data = {
+        merchant_id: 'd53a145f-b2c5-4dc5-afc0-cced9493aecf',
+        amount: this.getVipPrice(req.body.plan),
+        callback_url: 'http://localhost:3000/vip/payment/checker',
+        description: `افزایش اعتبار عضویت ویژه`,
+        metadata: {
+          email: req.user.email,
+        }
+      };
+
+      const requestHeader = {
+        'contetn-type': 'application/json',
+        'cache-control' : 'no-cache',
+      };
+
+      axios({
+        method: 'post',
+        headers: requestHeader,
+        url: 'https://api.zarinpal.com/pg/v4/payment/request.json',
+        data,
+      })
+      .then(async (response) => {
+        const data = response.data.data;
+        if(data.code === 100) {
+          const payment = new Payment({
+            user: req.user._id,
+            vip: true,
+            price: this.getVipPrice(req.body.plan),
+            status: 100,
+            resNumber: data.authority,
+          });
+          await payment.save();
+          return res.redirect(`https://www.zarinpal.com/pg/StartPay/${data.authority}`);
+        }
+        
+        return this.error(response.data.error);
+      })
+      .catch(error => next(error));
+    } catch (error) {
+      next(error);
+    }
+  }
+
 }
 
 module.exports = new UserController();
